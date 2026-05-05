4c93e18d6e9dc2c1e47504f882c92caf11a19e14
B4Vendetta <128187034+BGADstudio@users.noreply.github.com>
2026-05-05 15:43:19 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-05T12:43:16.119Z
-Commit: Fix database connection timeout
+Timestamp: 2026-05-05T12:43:17.996Z
+Commit: Resolve null pointer exception in user service

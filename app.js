f1189d8de1558d1baa88dcd09f22ddcab7f1f934
B4Vendetta <128187034+BGADstudio@users.noreply.github.com>
2026-05-05 15:20:23 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-05T12:20:19.921Z
-Commit: Add export functionality for reports
+Timestamp: 2026-05-05T12:20:21.988Z
+Commit: Fix broken links in documentation

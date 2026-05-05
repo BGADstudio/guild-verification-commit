c165cd8941cecea06fd240500bf61fbc465360ca
B4Vendetta <128187034+BGADstudio@users.noreply.github.com>
2026-05-05 15:43:24 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-05T12:43:17.996Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-05-05T12:43:23.403Z
+Commit: Fix broken links in documentation

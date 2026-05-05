1e11120dc4c0bdd1f286bfa9de66866f2cecf606
B4Vendetta <128187034+BGADstudio@users.noreply.github.com>
2026-05-05 15:43:41 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-05T12:43:35.660Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-05-05T12:43:39.574Z
+Commit: Add export functionality for reports

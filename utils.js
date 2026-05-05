54c5c62eb12801e4ee93eb9282ec18e453ff6c9b
B4Vendetta <128187034+BGADstudio@users.noreply.github.com>
2026-05-05 15:43:10 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-05T12:43:06.667Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-05-05T12:43:08.509Z
+Commit: Fix CSS styling bug on mobile devices

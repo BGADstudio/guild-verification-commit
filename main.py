2b6259377d7813920a6220026274702473953f56
B4Vendetta <128187034+BGADstudio@users.noreply.github.com>
2026-05-05 15:19:48 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-05T12:19:30.447Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-05-05T12:19:47.079Z
+Commit: Fix broken links in documentation

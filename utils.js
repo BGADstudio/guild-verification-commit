f4a0436237bf3bc4b8b82edb08315af0c03dd577
B4Vendetta <128187034+BGADstudio@users.noreply.github.com>
2026-05-05 15:19:50 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-05T12:19:45.357Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-05-05T12:19:48.962Z
+Commit: Introduce real-time notifications

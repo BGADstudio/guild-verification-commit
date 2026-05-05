7006e58e6d3cd215cab0234ffcfc0a34b55f934d
B4Vendetta <128187034+BGADstudio@users.noreply.github.com>
2026-05-05 15:43:45 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-05T12:43:39.574Z
-Commit: Add export functionality for reports
+Timestamp: 2026-05-05T12:43:44.449Z
+Commit: Resolve null pointer exception in user service
